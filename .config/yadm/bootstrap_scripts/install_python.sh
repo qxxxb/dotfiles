f66697bef4c64f7b6cfd24bf43a6d978a34caf53
qxxxb <46016483+qxxxb@users.noreply.github.com>
2020-12-21 19:22:58 -0500

    Run pyenv install

--- .config/yadm/bootstrap_scripts/install_python.sh
+++ .config/yadm/bootstrap_scripts/install_python.sh
@@ -7,9 +7,9 @@ sudo apt install -y make build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
     libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
-pyenv --skip-existing install 3.9.1
-pyenv --skip-existing install 2.7.18
-pyenv --skip-existing install pypy3.6-7.3.1
+pyenv install 3.9.1
+pyenv install 2.7.18
+pyenv install pypy3.6-7.3.1
 
 pyenv global 3.9.1
 
