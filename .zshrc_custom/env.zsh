11591a480bb1333c81654beffb3e22cb839cc3a7
qxxxb <46016483+qxxxb@users.noreply.github.com>
2020-12-22 17:36:26 -0500

    Reorder env.zsh

--- .zshrc_custom/env.zsh
+++ .zshrc_custom/env.zsh
@@ -6,14 +6,14 @@ export VISUAL=$(which nvim)
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 if [[ $EUID -ne 0 ]]; then
-    # If not root, use pyenv
-    export PATH=$HOME/.pyenv/bin:$PATH
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+    # If not root, use pyenv
+    export PATH=$HOME/.pyenv/bin:$PATH
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
 fi
