c295645ec3b28186e811084efbb6ae3fe74ecef4
qxxxb <46016483+qxxxb@users.noreply.github.com>
2021-01-14 21:53:52 -0500

    Minimize

--- .config/yadm/bootstrap_scripts/install_icons.sh
+++
@@ -1,8 +0,0 @@
-sudo add-apt-repository ppa:papirus/papirus
-
-sudo apt-get install \
-    papirus-icon-theme \
-    papirus-folders \
-    libreoffice-style-breeze
-
-papirus-folders -C yaru --theme Papirus-Dark
--- .zshrc_custom/env.zsh
+++ .zshrc_custom/env.zsh
@@ -4,31 +4,3 @@ export PATH=$HOME/Programs/static/bin:$PATH
 export VISUAL=$(which nvim)
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-# If not root, do this
-if [[ $EUID -ne 0 ]]; then
-    export NVM_DIR="$HOME/.nvm"
-    function load_nvm() {
-        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-        [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-    }
-
-    async_start_worker nvm_worker -n
-    async_register_callback nvm_worker load_nvm
-    async_job nvm_worker sleep 0.1
-
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-    function load_pyenv() {
-        eval "$(pyenv init -)"
-        eval "$(pyenv virtualenv-init -)"
-    }
-
-    async_start_worker pyenv_worker -n
-    async_register_callback pyenv_worker load_pyenv
-    async_job pyenv_worker sleep 0.1
-fi
-
-autoload -U +X bashcompinit && bashcompinit
-complete -o nospace -C /usr/bin/terraform terraform
