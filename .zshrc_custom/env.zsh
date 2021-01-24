5287de204b85bd6f5fdb99b298fb039c6a1b2b1a
qxxxb <46016483+qxxxb@users.noreply.github.com>
2021-01-24 01:01:45 -0500

    Add go

--- .zshrc_custom/env.zsh
+++ .zshrc_custom/env.zsh
@@ -1,6 +1,7 @@
 export TERM=xterm-256color
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/Programs/static/bin:$PATH
+export PATH=$HOME/Programs/static/go/bin:$PATH
 export VISUAL=$(which nvim)
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
